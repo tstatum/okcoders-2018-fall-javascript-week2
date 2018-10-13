3e8661e57fc1aa8f3b4a5618761c662066dcd8bd
Trinity Statum <tj.statum@gmail.com>
2018-10-13 09:43:24 -0500

    gto 8

--- example1.js
+++ example1.js
@@ -1,6 +1,12 @@
-var message = "Hello World"
+var message = "This is statement"
 console.log(message + " " + 1);
 console.log(message + " " + 2);
 console.log(message + " " + 3);
 console.log(message + " " + 4);
+console.log(message + " " + 5);
+console.log(message + " " + 6);
+console.log(message + " " + 7);
+console.log(message + " " + 8);
+
+
 
